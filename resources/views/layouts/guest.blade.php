0b3e1de247c527b9f3b7d46381393fabce2dc188
MohamedBerik <monsieur.atef@gmail.com>
2026-01-24 00:39:56 +0200

    update

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -9,11 +9,12 @@
     <meta name="author" content="TemplateMo">
     <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900" rel="stylesheet">
 
+    {{-- <link rel="stylesheet" href="{{ asset('assets/css/fontawesome.css') }}"> --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css"
         integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-    <title>Education Meeting HTML5 Template</title>
+    <title>Education Meeting</title>
 
     <!-- Bootstrap core CSS -->
     {{-- <link href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet"> --}}
@@ -22,13 +23,12 @@
 
 
     <!-- Additional CSS Files -->
-    {{-- <link rel="stylesheet" href="{{ asset('assets/css/fontawesome.css') }}"> --}}
     {{-- <link rel="stylesheet" href="{{ asset('assets/css/templatemo-edu-meeting.css') }}"> --}}
     <link rel="stylesheet" href="https://edumeeting-production.up.railway.app/assets/css/templatemo-edu-meeting.css">
+    {{-- <link rel="stylesheet" href="{{ asset('assets/css/owl.css') }}"> --}}
     <link rel="stylesheet" href="https://edumeeting-production.up.railway.app/assets/css/owl.css">
+    {{-- <link rel="stylesheet" href="{{ asset('assets/css/lightbox.css') }}"> --}}
     <link rel="stylesheet" href="https://edumeeting-production.up.railway.app/assets/css/lightbox.css">
-    <link rel="stylesheet" href="{{ asset('assets/css/owl.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/lightbox.css') }}">
 
 </head>
 
@@ -70,7 +70,7 @@
                                     </li>
                                 @else
                                     <li>
-                                        <a href="{{ route('login') }}">Log in</a>
+                                        <a class="btn" href="{{ route('login') }}">Log in</a>
                                     </li>
                                     @if (Route::has('register'))
                                         <li>
