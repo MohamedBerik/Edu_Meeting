aa4af88eccc0c3bc0d5eb178f84d63fe7b70b1fd
MohamedBerik <monsieur.atef@gmail.com>
2026-01-26 17:17:20 +0200

    fix

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -141,7 +141,15 @@
             });
         });
     </script>
+    <style>
+        .nav .sub-menu {
+            display: none;
+        }
 
+        .nav li.open>.sub-menu {
+            display: block;
+        }
+    </style>
     {{-- <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script> --}}
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous">
