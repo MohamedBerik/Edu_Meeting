544c56ed7cb5333972e4ef5ab78b6819c477d21b
MohamedBerik <monsieur.atef@gmail.com>
2026-01-26 17:04:16 +0200

    fix files

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -129,6 +129,19 @@
     <!-- Scripts -->
     <!-- Bootstrap core JavaScript -->
     <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
+
+    <script>
+        $(document).ready(function() {
+            $('.nav li.has-sub > a').on('click', function(e) {
+                if ($(window).width() < 992) {
+                    e.preventDefault();
+                    $(this).parent().toggleClass('open');
+                    $(this).next('.sub-menu').slideToggle(300);
+                }
+            });
+        });
+    </script>
+
     {{-- <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script> --}}
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous">
